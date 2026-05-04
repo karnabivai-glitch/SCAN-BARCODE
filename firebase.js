98c400ff834189344406b44c4b365ef80193150f
karnabivai-glitch <karnabivai@gmail.com>
2026-05-04 09:39:49 +0700

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDpcYIgpUhU2kKnZiHCc1fVJmVjDx_VzXo",
-  authDomain: "scan-barcode-7bb05.firebaseapp.com",
-  projectId: "scan-barcode-7bb05",
-};
-
-export const app = initializeApp(firebaseConfig);
